4b7690f3061fdcbbee5ba7a624e2bf9367c09c31
Deven96 <diretnandomnan@gmail.com>
2019-02-02 23:14:16 +0100

    moved scripts into their folder

---
+++ scripts/post_deploy_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install python package
+pip install search-engine-parser
+
+# run the cli version to get a result
+python -m search_engine_parser.core.cli --query "Preaching to the choir" --engine bing --type descriptions
+
+if [$? -eq 0]; then
+    echo "Package works as expected"
+else
+    echo "CLI handler of the package failed to execute"
+fi
