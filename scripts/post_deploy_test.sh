50f5549d920cec75656dd1ff2f65d3f28f484549
Deven96 <diretnandomnan@gmail.com>
2019-02-03 00:26:43 +0100

    post deploy test

--- scripts/post_deploy_test.sh
+++ scripts/post_deploy_test.sh
@@ -6,7 +6,7 @@ pip install search-engine-parser
 # run the cli version to get a result
 python -m search_engine_parser.core.cli --query "Preaching to the choir" --engine bing --type descriptions
 
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     echo "Package works as expected"
 else
     echo "CLI handler of the package failed to execute"
