8a2d115077d26e4bac9ad34c1d1f72ec3b7c8956
Diretnan Domnan <diretnandomnan@gmail.com>
2019-09-04 18:06:19 +0100

    creating dev branch

--- search_engine_parser/core/engines/aol.py
+++ search_engine_parser/core/engines/aol.py
@@ -10,18 +10,18 @@ class AolSearch(BaseSearch):
     Searches Aol for string
     """
     name = "AOL"
-    search_url = "https://search.aol.com/aol/search?q={query}"
+    search_url = "https://search.aol.com/aol/search?q={query}&page={page}"
     summary = "\t According to netmarketshare, the old time famous AOL is still in the top 10 "\
-	      "search engines with a market share that is close to 0.06%. \n\ "\
-	      "The AOL network includes many popular web sites like engadget.com, techchrunch.com and the huffingtonpost.com. \n\ "\
+	      "search engines with a market share that is close to 0.06%. "\
+	      "The AOL network includes many popular web sites like engadget.com, techchrunch.com and the huffingtonpost.com. \n"\
               "On June 23, 2015, AOL was acquired by Verizon Communications."
     
-    def search(self, soup):
+    def parse_soup(self, soup):
         """
         Parses AOL for a search query
         """
         # find all divs
-        return soup.find.all('div', class_='algo-sr')
+        return soup.find_all('div', class_='algo-sr')
 
     def parse_single_result(self, single_result):
         """
@@ -31,10 +31,10 @@ class AolSearch(BaseSearch):
         :return: parsed title, link and description of single result
         :rtype: str, str, str
         """
-        h2 = single_result.find('h2')
-        link_tag = h2.find('a')
+        h3 = single_result.find('h3')
+        link_tag = h3.find('a')
         caption = single_result.find('div', class_='compText aAbs')
-        desc = caption.find('lh-16')
+        desc = caption.find('p', class_='lh-16')
         ''' Get the text and link '''
         title = link_tag.text
 
--- search_engine_parser/tests/test_search.py
+++ search_engine_parser/tests/test_search.py
@@ -6,7 +6,7 @@ from search_engine_parser.core import (
     YahooSearch, GoogleSearch, BingSearch, DuckDuckGoSearch, AolSearch
 )
 
-search_args = ('preaching to the choir', 1)
+search_args = ('preaching to the choir', 2)
 
 
 def test_yahoo_search():
@@ -18,22 +18,18 @@ def test_yahoo_search():
 def test_google_search():
     engine = GoogleSearch()
     results = engine.search(*search_args)
-    assert len(results['titles']) == 10
+    assert len(results['titles']) >= 9
 
 
 def test_bing_search():
     engine = BingSearch()
-    # change search args because bing first page always includes video
-    bing_search_args = ('preaching to the choir', 2)
-    results = engine.search(*bing_search_args)
+    results = engine.search(*search_args)
     assert len(results['titles']) == 10
 
 
 def test_aol_search():
     engine = AolSearch()
-    # change search args because aol first page always includes video
-    aol_search_args = ('preaching to the choir', 2)
-    results = engine.search(*aol_search_args)
+    results = engine.search(*search_args)
     assert len(results['titles']) == 10
 
 
