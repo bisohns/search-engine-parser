cb3510bf9a44981d75eb47eb321a35ce90fdf320
PalAditya <adityapal.nghss@gmail.com>
2019-09-14 10:40:00 +0530

    Implementing requested PR changes

--- search_engine_parser/core/__init__.py
+++ search_engine_parser/core/__init__.py
@@ -1,11 +1,13 @@
 import sys, os
 
+import sys, os
+
 from search_engine_parser.core.engines import ( YahooSearch, 
-												GoogleSearch, 
-												BingSearch, 
-												DuckDuckGoSearch, 
-												AolSearch, 
-												YandexSearch,
-												StackOverflowSearch,
-												BaiduSearch
+                                                GoogleSearch, 
+                                                BingSearch, 
+                                                DuckDuckGoSearch, 
+                                                AolSearch, 
+                                                YandexSearch,
+                                                StackOverflowSearch,
+                                                BaiduSearch
 )
--- search_engine_parser/core/engines/baidu.py
+++ search_engine_parser/core/engines/baidu.py
@@ -8,6 +8,7 @@ class BaiduSearch(BaseSearch):
 	"""
 	Searches Baidu for string
 	"""
+	offset=None
 	name = "Baidu"
 	search_url = "https://www.baidu.com/s?wd={query}&pn={offset}&oq={query}"
 	summary = "\tBaidu, Inc. is a Chinese multinational technology company specializing in"\
@@ -16,15 +17,32 @@ class BaiduSearch(BaseSearch):
 	" companies in the world.\n\tBaidu offers various services, including a"\
 	" Chinese search engine, as well as a mapping service called Baidu Maps."
 
+	
+	"""Override get_search_url"""
+	def get_search_url(self, query=None, page=None):
+		""" 
+		Return a formatted search url.
+		Offsets are of form 0,10,20, etc. So if 1 is passed, we make it 0, for 2->(2-1)*10=10. etc.
+		Adding offset value to self as we would need it in parse_soup
+		"""
+		
+		self.offset = (page - 1) * 10
+		return  self.search_url.format(query=query, page=page, offset=self.offset)
+	
+	
 	def parse_soup(self, soup):
 		"""
 		Parses Baidu for a search query
 		"""
 		
-		"""Baidu search can be made deterministic via an id, so add it in self"""
+		"""Baidu search can be made deterministic via an id.
+		The id of divs are in range 1-10 for page 1, 11-20 in page 2, so that arithmetic is
+		handled by the previous calculation of offset (it transforms 1 to 0, 2 to 10, etc.)
+		and we loop for all div's from (10+1==11) to (10+11-1)=20 {-1 due to nature of range function}
+		"""
 		
 		soups=[]
-		for i in range(int(self.offset)*10+1,int(self.offset)*10+11):
+		for i in range(int(self.offset) + 1,int(self.offset) + 11):
 			soups.append(soup.find('div',id=i))
 
 		return soups
