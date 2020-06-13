ef1e8d5541b96d43171f160eb62b7e5cb3af57b5
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-06-13 19:48:21 +0100

    Bumped Version to 0.6

--- search_engine_parser/__init__.py
+++ search_engine_parser/__init__.py
@@ -38,4 +38,4 @@ from search_engine_parser.core.engines.stackoverflow import \
 from search_engine_parser.core.engines.yahoo import Search as YahooSearch
 
 name = "search-engine-parser"  # pylint: disable=invalid-name
-__version__ = "0.5.4"
+__version__ = "0.6"
